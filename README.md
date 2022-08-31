5e0a8fe1bc8c4f94b84ad769c5f4ffe938e6161d
Anders Åslund <anders.aslund@teknoir.ai>
2022-08-31 11:34:25 +0200

    Work in progress

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# device-patch_artifact-registry
-A Kubeflow pipeline and an Ansible playbook to patch devices adding Artifact Registry pull secret
+# device-patch-base
+A Kubeflow pipeline that run an Ansible playbook to patch devices
+
+## Pipeline input
+
+```json
+{
+  "playbook_repo": "<git repo with a playbook>",
+  "playbook_path": "<path to playbook in repo>",
+  "ansible_limit": "<ansible limit>"
+}
+```
+
+### Ansible limit
+A limit is used, following the patterns in Ansible, to select devices to run the patch on.
+Se reference doc here:
+[https://docs.ansible.com/ansible/latest/user_guide/intro_patterns.html](https://docs.ansible.com/ansible/latest/user_guide/intro_patterns.html)
+
+> **The Teknoir inventory:**
+> Namespaces/labels become groups, Ansible do not support namespaces/labels with dashes(-).
+> Dashes(-) will be replaced with underscores(_).
+> Remember that when using them!
+
+## Patching "technique"
+Devices might not be accessible, for different reasons.
+There is a technique to make sure the patch only runs once per device.
+By matching limit and when finished modifying device labels...TBD
+Schedule recurrant job
---
+++ run_playbook.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+set -eo pipefail
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -l|--limit)
+    LIMIT="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -r|--repo)
+    REPO="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -p|--playbook)
+    PLAYBOOK="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -h|--help|*)
+    echo "$0 -r(--repo) <repo url> -p(--playbook) <path to playbook in repo> -l(--limit) <ansible limit string>"
+    echo "\nExample:"
+    echo "$0 --repo https://github.com/teknoir/device-patch-base.git --playbook example/patch_device_example.yaml --limit 'hardware=nvidia-jetson-nano-b00,patch_version notin (verified)'"
+    echo ""
+    echo ""
+    exit 0
+    ;;
+esac
+done
+
+DEVICES=$(kubectl -l ${LIMIT} get device -o name)
+
+for DEVICE in ${DEVICES} ; do
+  echo "Enabling tunnel for: ${DEVICE}"
+  TUNNEL_PORT="$(kubectl get $DEVICE -o jsonpath='{.spec.keys.data.tunnel}' | base64 -d)"
+  re='^[0-9]+$'
+  if ! [[ $TUNNEL_PORT =~ $re ]] ; then
+    TUNNEL_PORT="$(( ( RANDOM % 64511 )  + 1024 ))"
+    TUNNEL_PORT_B64=$(echo -ne "${TUNNEL_PORT}" | base64)
+    kubectl patch ${DEVICE} \
+      --type merge \
+      -p "{\"spec\":{\"keys\":{\"data\":{\"tunnel\":\"${TUNNEL_PORT_B64}\"}}}}"
+  fi
+done
+
+sleep 5m
+
+git clone ${REPO} ./playbook_repo
+ansible-playbook -v ./playbook_repo/${PLAYBOOK} --limit ${LIMIT} || true
+
+
+for DEVICE in ${DEVICES} ; do
+  echo "Disabling tunnel for ${DEVICE}"
+  kubectl patch ${DEVICE} \
+    --type merge \
+    -p "{\"spec\":{\"keys\":{\"data\":{\"tunnel\":\"TkE=\"}}}}"
+done
